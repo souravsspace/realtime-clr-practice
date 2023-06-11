426096b63ab1eeb8ace2a1fe17ac78cd4cd7a2c3
Sourav <sourav@Souravs-MacBook-Air.local>
2023-06-12 03:24:57 +0600

    done

---
+++ app.js
@@ -0,0 +1,12 @@
+
+// let logo = document.getElementsByClassName("logo")[0];
+
+// const onMouseOut = (e) =>{
+//   e.target.classList.toggle("againLogo");
+// }
+
+// logo.addEventListener("click", onMouseOut)
+
+
+
+
